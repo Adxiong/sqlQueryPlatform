3d206c171d1adeb43a44a8dd478f670e94cfe98f
adxiong <260245435@qq.com>
2022-01-18 23:59:45 +0800

    database 查询返回数据类型修改

--- server/src/dao/database.ts
+++ server/src/dao/database.ts
@@ -4,16 +4,24 @@
  * @Author: Adxiong
  * @Date: 2022-01-18 23:28:15
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-18 23:31:06
+ * @LastEditTime: 2022-01-18 23:56:04
  */
 
 
 import pool from "./pool";
-
+import { DatabaseBaseType } from "../types/database";
 class DatabaseDao {
- async queryDatabases (): Promise<object> {
+ async queryDatabases (): Promise<DatabaseBaseType[]> {
   const sql = 'show databases'
-  return await pool.query(sql, [])
+  const data =  await pool.query(sql, [])  
+  const result = []
+  data.forEach( (item: { Database: string },index) => {
+    result.push({
+      id: index,
+      name: item.Database
+    })
+  })  
+  return result
  }
 }
 
---
+++ server/src/types/database.ts
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Adxiong
+ * @Date: 2022-01-18 23:51:27
+ * @LastEditors: Adxiong
+ * @LastEditTime: 2022-01-18 23:55:53
+ */
+
+export interface DatabaseBaseType {
+  id: number;
+  name: string;
+}
