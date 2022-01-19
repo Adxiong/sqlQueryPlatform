73e25722a462b493206f95f239801a0191f55941
adxiong <260245435@qq.com>
2022-01-19 23:51:37 +0800

    接口返回格式规范化

--- server/src/controller/database.ts
+++ server/src/controller/database.ts
@@ -4,18 +4,19 @@
  * @Author: Adxiong
  * @Date: 2022-01-18 23:26:05
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-19 23:19:42
+ * @LastEditTime: 2022-01-19 23:43:33
  */
 
 import { Router, Response, Request, NextFunction } from 'express';
 import DatabaseService from "../service/database";
+import { ApiResult, ResponseStatus} from '../utils/apiResult';
 
 const router =  Router()
 
 router.get("/list", (req: Request, res: Response, next: NextFunction) => {
   DatabaseService.queryDatabases()
   .then( data => {
-    res.json(data)
+    res.json(new ApiResult(ResponseStatus.success, data))
   })
   .catch(next)
 }) 
@@ -23,12 +24,12 @@ router.get("/list", (req: Request, res: Response, next: NextFunction) => {
 
 router.get('/createDatabase', (req: Request, res: Response, next: NextFunction) => {
   if (!req.body.name) {
-    res.json()
+    res.json(new ApiResult(ResponseStatus.fail, null, '数据不完整'))
     return
   }
   DatabaseService.createDatabase(req.body.name)
   .then( () => {
-    res.json()
+    res.json(new ApiResult(ResponseStatus.success, null, null))
   })
   .catch(next)
 })
