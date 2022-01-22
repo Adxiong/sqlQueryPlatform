0e72b297eb0316773f4eebea79d0e4a151739b93
adxiong <260245435@qq.com>
2022-01-22 20:11:25 +0800

    card style edit

--- webapp/src/components/card/card.tsx
+++ webapp/src/components/card/card.tsx
@@ -4,11 +4,11 @@
  * @Author: Adxiong
  * @Date: 2022-01-21 13:39:30
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-22 17:28:30
+ * @LastEditTime: 2022-01-22 20:05:55
  */
 import { FC } from "react";
 import styles from "../card/card.module.less"
-
+import mysqlPng from "../../assets/img/mysql.png"
 export interface CardDataType {
   title: string ;
   img: string ;
@@ -19,18 +19,32 @@ interface Props {
   data: CardDataType[]
 }
 
+interface logoMapType {
+  [propKey: string]: string
+}
+
+const logoMap: logoMapType = {
+  "mysql": mysqlPng
+}
+
 const Card: FC<Props> = (props) => {
   const { data, children } = props
+  
   return (
     <div id={styles.Card}>
       {
         data.map( (item: CardDataType) => {
           return (   
             <div className={styles.cardItem}>
-              <img src={item.img} alt={item.desc} />
-              <span className="title">{item.title}</span>
-              <div className="tools">
-                {children}
+              <img src={logoMap[item.title]} alt={item.desc} />
+              <div className={styles.content}>
+                <span className={styles.title}>{item.title}</span>
+                <div className={styles.tools}>
+                  {/* {children} */}
+                  <div>1</div>
+                  <div>s</div>
+                  <div>w</div>
+                </div>
               </div>
             </div>
           )
--- webapp/src/pages/home/home.tsx
+++ webapp/src/pages/home/home.tsx
@@ -4,7 +4,7 @@
  * @Author: Adxiong
  * @Date: 2022-01-16 18:31:37
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-01-22 17:53:19
+ * @LastEditTime: 2022-01-22 19:42:04
  */
 import Description from "../../components/description/description"
 import React from "react";
@@ -17,7 +17,6 @@ interface Props {
 const Home: React.FC<Props> = (props) => {
   const data = new Array(12).fill({
     title: "mysql",
-    img: '',
     desc: '数据库'
   })
   const labelWidth = 100
