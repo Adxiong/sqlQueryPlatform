beace78e4e7eec70a5ec48e3f69b6bc0c8eafd6d
adxiong <260245435@qq.com>
2022-02-07 20:54:37 +0800

    增加配置修改组件

---
+++ webapp/src/pages/configPage/alterConfig.tsx
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: Adxiong
+ * @Date: 2022-02-07 20:53:19
+ * @LastEditors: Adxiong
+ * @LastEditTime: 2022-02-07 20:53:19
+ */
+
+import React, { FC } from 'react'
+
+interface Props {
+
+}
+const AlterConfig: FC = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AlterConfig
